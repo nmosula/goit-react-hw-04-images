81daa3fcffc8da837c3ca563fd6a2b10d06c3835
Nazar Mosula <nmosula@gmail.com>
2023-03-05 13:59:46 +0200

    Update getImages

--- src/services/getImages.jsx
+++ src/services/getImages.jsx
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const BASE_URL = "https://pixabay.com/api/";
 const AXIOS_KEY = "32870286-1027cf9b16afcfb20a632269d";
-const PER_PAGE = 12;
+const PER_PAGE = 40;
+const PAGE = 1;
     
 export const getImages = async (searchQuery, page) => {
 
-    const response = await axios.get(`${BASE_URL}?key=${AXIOS_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`);
+    const response = await axios.get(`${BASE_URL}?key=${AXIOS_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${PAGE}&per_page=${PER_PAGE}`);
     const images = response.data.hits.map(
     ({ id, webformatURL, largeImageURL, tags }) => {
       return {
@@ -20,3 +21,4 @@ export const getImages = async (searchQuery, page) => {
   return { images, totalImages: response.data.totalHits };
    
 }
+
